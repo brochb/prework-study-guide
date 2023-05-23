67975b55d7749c5ebcb07a929ab5cdf796fee62a
Brochb <brochbaltzer@gmail.com>
2023-05-22 18:16:15 -0600

    fleshed out README file

--- assets/script.js
+++ assets/script.js
@@ -2,12 +2,15 @@ var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 
 var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
+//This function lists the topics array of subjects to be studied
 function listTopics() {
     for (var x = 0; x < topics.length; x++) {
         console.log(topics[x]);
     }
 }
 
+
+//This function selects the topic to study randomly from among the topics outlines in the function above
 function selectTopic() {
     if (randomTopic === 'HTML') {
         console.log("Let's study HTML!");
@@ -22,6 +25,8 @@ function selectTopic() {
     }
 }
 
+
+//This is where the console.log commands will all go
 console.log('Here are the topics we learned throughout Prework:');
 listTopics();
 console.log('Which topic should we study first?');
